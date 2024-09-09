e26d2d6bf57d10a3813b51fdad41c3457a175ad5
milton <macastro7@tes.edu.ec>
2024-09-09 15:51:15 -0500

    Subida inicial de archivos de WordPress

---
+++ backup_2024-09-09-1059_ToCupboard_bd29b8977fa6-others/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1726427819;s:7:"authKey";s:64:"=9w8DgBh.6LG7*OG7r3[bu{_[IoI 9?*e p<8Om/e5~VwAv14pWw_D 7)FCXUc`M";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:1904;}
